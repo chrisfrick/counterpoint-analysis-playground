8eac819e48b4784b841608507c0639aee7e1a9e2
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-09-13 16:14:18 -0400

    work on usesMostlyStepwiseMotion and workshop error formatting

--- src/melodic-analysis.mjs
+++ src/melodic-analysis.mjs
@@ -1,4 +1,4 @@
-import { Scale, Note, Range } from 'tonal';
+import { Scale, Note, Range, Interval } from 'tonal';
 
 /* Input melody */
 
@@ -46,7 +46,7 @@ const melody = {
     {
       notes: [
         {
-          pitch: 'C5',
+          pitch: 'C4',
           duration: 'w',
         },
       ],
@@ -60,7 +60,10 @@ const usesCadenceFormula = (notes, key) => {
 
   // Check that last note is 'do'
   if (Note.pitchClass(lastThreeNotes[2]) !== scale(1)) {
-    return false;
+    return {
+      severity: 0,
+      type: 'does not end on tonic',
+    };
   }
 
   const scaleInCorrectOctave = Scale.degrees(lastThreeNotes[2] + ' major');
@@ -90,10 +93,13 @@ const usesCadenceFormula = (notes, key) => {
       compareArrays(formula, lastThreeNotes)
     )
   ) {
-    return true;
+    return { severity: 0 };
   }
 
-  return false;
+  return {
+    severity: 2,
+    type: 'does not use a proper cadence formula',
+  };
 };
 
 const rangeIsWithinAnOctave = (notes) => {
@@ -101,8 +107,61 @@ const rangeIsWithinAnOctave = (notes) => {
   const max = rangeArr.reduce((max, cur) => Math.max(max, cur));
   const min = rangeArr.reduce((min, cur) => Math.min(min, cur));
   const range = max - min;
-  if (range <= 12) return true;
-  return false;
+  if (range <= 12) {
+    return {
+      severity: 0,
+    };
+  }
+  if (range > 12 && range <= 16) {
+    return {
+      severity: 1,
+      type: 'range is larger than an octave',
+    };
+  }
+  if (range > 16) {
+    return {
+      severity: 3,
+      type: 'range is significantly larger than an octave',
+    };
+  }
+};
+
+const usesMostlyStepwiseMotion = (melody) => {
+  let prevNote;
+  let currentNote;
+  let melodicIntervals = [];
+
+  melody.measures.forEach((measure, measureIndex, measureArr) => {
+    measure.notes.forEach((note, noteIndex, noteArr) => {
+      if (measureIndex === 0 && noteIndex === 0) {
+        currentNote = melody.measures[0].notes[0];
+      } else {
+        prevNote = currentNote;
+        currentNote = noteArr[noteIndex];
+        let distance = Interval.distance(prevNote.pitch, currentNote.pitch);
+        melodicIntervals.push(distance);
+      }
+    });
+  });
+
+  let melodicIntervalNumbers = melodicIntervals.map((interval) =>
+    Math.abs(Interval.get(interval).num)
+  );
+
+  let intervalCount = melodicIntervalNumbers.length;
+  let leapsCount = melodicIntervalNumbers.reduce(
+    (leaps, interval) => (interval > 2 ? leaps + 1 : leaps),
+    0
+  );
+
+  console.log(leapsCount / intervalCount);
+};
+
+const errorFormat = {
+  measureNumber: 1,
+  noteIndex: 1,
+  severity: 3, // something like 1 low, 2 med, 3 high?
+  type: 'leap should resolve by step', // consecutive leaps, range is greater than octave
 };
 
 // TODO: Implement types here!
@@ -119,6 +178,7 @@ const melodicAnalysis = (melody) => {
   console.log('cadence formula:', usesCadenceFormula(notes, 'C'));
   console.log('range is within an octave', rangeIsWithinAnOctave(notes));
   // Check for mostly stepwise motion
+  usesMostlyStepwiseMotion(melody);
   // Check for leaps >= 4th to reverse by step
   // Check for consecutive leaps
 
