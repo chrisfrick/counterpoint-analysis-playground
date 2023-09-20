652283d5d8885a52ab57cb403c6225565a34b9b1
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-09-20 09:36:36 -0400

    serve react app with via express server, deployment files added for fly

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static('frontend/build'));
+app.use(express.static('dist'));
 
 app.get('/health', (req, res) => {
   res.send('ok');
