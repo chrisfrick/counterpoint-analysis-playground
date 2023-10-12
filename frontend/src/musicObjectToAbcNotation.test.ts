dea5c291f7ccc599b94417ed6ded400fa59aa548
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-10-12 16:53:50 -0400

    add tests

--- frontend/src/musicObjectToAbcNotation.test.ts
+++ frontend/src/musicObjectToAbcNotation.test.ts
@@ -43,4 +43,18 @@ w: P8 M6 m3 M3 P8 A4 P5 M6 P8
 [V: v2] C,1/2yD,1/2y|E,1/2yF,1/2y|G,1/2yF,1/2y|E,1/2yD,1/2y|C,1y|]
 `);
   });
+
+  test('returns appropriate string when checkTritones=false, showMotion=false', () => {
+    expect(firstSpeciesToAbc(musicWithTritoneError, false, false)).toEqual(`
+M: 4/4
+L: 1
+K: C
+%%staves [v1, v2]
+V: v1 clef="treble"
+V: v2 clef="bass"
+[V: v1] c1/2yB1/2y|G1/2yA1/2y|G1/2yB1/2y|B1/2yB1/2y|c1y|]
+w: P8 M6 m3 M3 P8 A4 P5 M6 P8
+[V: v2] C,1/2yD,1/2y|E,1/2yF,1/2y|G,1/2yF,1/2y|E,1/2yD,1/2y|C,1y|]
+`);
+  });
 });
